c1456e54283fc3734edb923830f9ec1f3f14835c
Davi <davifariaroch@gmail.com>
2023-03-09 19:27:22 -0300

    novo

---
+++ app/src/main/java/com/example/agenda/AgendaTelaInicial.java
@@ -0,0 +1,18 @@
+package com.example.agenda;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AgendaTelaInicial extends AppCompatActivity {
+privete Button sundays, mondays, tuesdays, wednesdays, thursdays, fridays, saturdays,
+    sundays, mondays, tuesdays, wednesdays, thursdays, fridays, saturdays,
+    sundays, mondays, tuesdays, wednesdays, thursdays, fridays, saturdays,
+    sundays, mondays, tuesdays, wednesdays, thursdays, fridays, saturdays,
+    sundays, mondays, tuesdays, wednesdays, thursdays, fridays, saturdays,
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_agenda_tela_inicial);
+    }
+}
