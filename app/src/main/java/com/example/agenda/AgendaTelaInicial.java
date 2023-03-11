f35f88c4301340f1857a08aff607cf133562845a
Davi <davifariaroch@gmail.com>
2023-03-11 20:55:25 -0300

    inicio das mudanças para adicionar a biblioteca Calendar view

--- app/src/main/java/com/example/agenda/AgendaTelaInicial.java
+++ app/src/main/java/com/example/agenda/AgendaTelaInicial.java
@@ -5,11 +5,11 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class AgendaTelaInicial extends AppCompatActivity {
-privete Button sundays, mondays, tuesdays, wednesdays, thursdays, fridays, saturdays,
+/*privete Button sundays, mondays, tuesdays, wednesdays, thursdays, fridays, saturdays,
     sundays, mondays, tuesdays, wednesdays, thursdays, fridays, saturdays,
     sundays, mondays, tuesdays, wednesdays, thursdays, fridays, saturdays,
     sundays, mondays, tuesdays, wednesdays, thursdays, fridays, saturdays,
-    sundays, mondays, tuesdays, wednesdays, thursdays, fridays, saturdays,
+    sundays, mondays, tuesdays, wednesdays, thursdays, fridays, saturdays,*/
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
